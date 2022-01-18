a0dc20f1c0bdd56d05215ce01620c9bb5074db52
Juan C. Bertoia <bertoia114@gmail.com>
2022-01-18 18:42:17 -0300

    Se empezo a agregarles estilos a la pagina.

--- nosotros.php
+++ nosotros.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/nosotros.css">
   <title>Sobre nosotros</title>
 </head>
 <body>
@@ -17,46 +18,53 @@
           <li><a href="#">Usuarios</a></li>
         </ul>
       </nav>
-      <h1>Sobre nosotros</h1>
+      <div class="portada">
+        <img src="" alt="" class="img_portada">
+        <h1>Sobre nosotros</h1>
+      </div>
     </header>
-    <img src="" alt="" class= "imagen_laboral">
-    <section>
-      <h2>Oportunidades laborales</h2>
- 
-      <p class = "parrafo1">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Vitae necessitatibus eos, mollitia minima fugiat, 
-        quo fuga iste officia adipisci ratione earum tempora magnam, 
-        nemo vero velit illo hic totam omnis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Vitae necessitatibus eos, mollitia minima fugiat, 
-        quo fuga iste officia adipisci ratione earum tempora magnam, 
-        nemo vero velit illo hic totam omnis.
-      </p>
-      <p class = "parrafo2">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Vitae necessitatibus eos, mollitia minima fugiat, 
-        quo fuga iste officia adipisci ratione earum tempora magnam, 
-        nemo vero velit illo hic totam omnis.
-      </p>
-      </section>
-      <img src="" alt="" class= "imagen_productos">
-      <article>
-        <h2>Productos</h2>
-        <p class="parrafo3">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Vitae necessitatibus eos, mollitia minima fugiat, 
-        quo fuga iste officia adipisci ratione earum tempora magnam, 
-        nemo vero velit illo hic totam omnis.
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-        Vitae necessitatibus eos, mollitia minima fugiat, 
-        quo fuga iste officia adipisci ratione earum tempora magnam, 
-        nemo vero velit illo hic totam omnis.
+    
+    <section class= "laboral">
+      <img src="" alt="" class= "imagen_laboral">
+      <article class="art_laboral">
+        <h2>Oportunidades laborales</h2>
+        <p class = "parrafo1">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Vitae necessitatibus eos, mollitia minima fugiat, 
+          quo fuga iste officia adipisci ratione earum tempora magnam, 
+          nemo vero velit illo hic totam omnis.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Vitae necessitatibus eos, mollitia minima fugiat, 
+          quo fuga iste officia adipisci ratione earum tempora magnam, 
+          nemo vero velit illo hic totam omnis.
+        </p>
+        <p class = "parrafo2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Vitae necessitatibus eos, mollitia minima fugiat, 
+          quo fuga iste officia adipisci ratione earum tempora magnam, 
+          nemo vero velit illo hic totam omnis.
         </p>
       </article>
-      <footer>
-        <h2 class="final">Curso PHP</h2>
-      </footer>
+    </section>
+    <section class="producto">
+      <img src="" alt="" class= "imagen_productos">
+      <article class="art_producto">
+        <h2>Productos</h2>
+        <p class="parrafo3">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Vitae necessitatibus eos, mollitia minima fugiat, 
+          quo fuga iste officia adipisci ratione earum tempora magnam, 
+          nemo vero velit illo hic totam omnis.
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Vitae necessitatibus eos, mollitia minima fugiat, 
+          quo fuga iste officia adipisci ratione earum tempora magnam, 
+          nemo vero velit illo hic totam omnis.
+        </p>
+      </article>
+    </section>
+    <footer>
+      <h2 class="final">Curso PHP</h2>
+    </footer>
   </div>
 </body>
 </html>

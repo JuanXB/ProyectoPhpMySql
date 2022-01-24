81dff37c85485565bfa4a8b9eea4aa49c61f8a87
Juan C. Bertoia <bertoia114@gmail.com>
2022-01-24 18:40:16 -0300

    Se empezo a agregarles estilos a la pagina.

--- contacto.php
+++ contacto.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/contacto.css">
   <title>Contacto</title>
 </head>
 <body>
@@ -11,25 +12,25 @@
   <header>
       <nav class="menu">
         <ul>
-          <li><a href="home.php">Inicio</a></li>
-          <li><a href="nosotros.php">Sobre nosotros</a></li>
-          <li><a href="#">Contacto</a></li>
+          <li><a href="home.php">Inicio</a>|</li>
+          <li><a href="nosotros.php">Sobre nosotros</a>|</li>
+          <li><a href="#">Contacto</a>|</li>
           <li><a href="usuarios.php">Usuarios</a></li>
         </ul>
       </nav>
     </header>
     <form action="datos_peticion.php" method="post">
       <h2>Contactanos</h2>
-      <label for="" name="nombre" >Ingresa tu nombre</label>
-      <input type="text" name="usuario" require>
+      <label for="" name="nombre">Ingresa tu nombre</label>
+      <input type="text" name="usuario"  placeholder="Nombre"  require>
 
       <label for="" name="usuario">Ingresa tu usuario</label>
-      <input type="text" name="usuario" require>
+      <input type="text" name="usuario"  placeholder="Usuario"  require>
 
-      <label for="" name="correo">Ingresa tu correo</label>
-      <input type="text" name="correo" require>
+      <label for="" name="correo" >Ingresa tu correo</label>
+      <input type="text" name="correo" placeholder="Correo electrónico" require>
 
-      <textarea name="peticion" id="" cols="30" rows="10"></textarea>
+      <textarea name="peticion" id="" cols="30" rows="10" placeholder="Ingrese su petición..."></textarea>
       <button>Enviar</button>
     </form>
   </div>
